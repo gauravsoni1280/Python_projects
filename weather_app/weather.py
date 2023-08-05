b02aeb8b00d4a3a51c7cd3eb377c3cb0a4ce8d86
Gaurav Soni <84252312+gauravsoni1280@users.noreply.github.com>
2023-08-05 16:31:16 +0530

    Update weather.py

--- weather_app/weather.py
+++ weather_app/weather.py
@@ -1,14 +1,15 @@
-
 import requests,json
 import spch_syn
 
 if __name__=='__main__':
     try:
         city=input("Enter Your City : ")
-        url=f"https://api.weatherapi.com/v1/current.json?key=fb935f08f31e40b8bf9211825231607&q={city}"
+        url=f"https://api.weatherapi.com/v1/current.json?key=1434e3c643804d2c8d2105338230508&q={city}"
         req=requests.get(url)
         weather_data=json.loads(req.text)
         temp_c=weather_data['current']['temp_c']
-        spch_syn.robosp(f'Greetings sir or mam , the current temperature of {city} is {temp_c} degree celsius')
+        condition=weather_data['current']['condition']['text']
+        print(weather_data)
+        spch_syn.robosp(f'Greetings sir or mam , the current temperature of {city} is {temp_c} degree celsius , and the weather condition is {condition}')
     except:
         spch_syn.robosp("Please enter a valid city name sir or mam")
